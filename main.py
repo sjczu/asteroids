c0f8ec1265a665ac4f06c3015c470947bd347e56
sjczu <sadoxz05@gmail.com>
2025-01-07 21:54:29 +0100

    v0.1 - initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+from constants import *
+
+def main():
+    print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    # screen.set_caption("Asteroids")
+    fpsClock = pygame.time.Clock()
+    while(True):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0,0,0))
+        pygame.display.flip()
+        
+
+if __name__ == "__main__":
+    main()
