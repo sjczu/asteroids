009ee80a2470f9ae04681d2d668610bbcf00f3f2
sjczu <sadoxz05@gmail.com>
2025-01-07 22:13:29 +0100

    v0.2 add: player class, circle rendering

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
@@ -7,12 +8,18 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     # screen.set_caption("Asteroids")
     fpsClock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
+
+        dt = fpsClock.tick(60)/1000
         
 
 if __name__ == "__main__":
